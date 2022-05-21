ad361cd1fade5f3bbcc809ca13569b7886efc3f3
yekerr <zixinh2@illinois.edu>
2022-05-21 18:13:44 -0500

    t

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #usr/bin/env bash
 
 # install CmdStan
-if [ ! -e cmdstan-2.16.0/bin/stanc ]; do
+if [ ! -e cmdstan-2.16.0/bin/stanc ]; then
     mkdir ./cmdstan-2.16.0; tar -xf cmdstan-2.16.0.tar.gz -C ./cmdstan-2.16.0 --strip-components=1
     cd ./cmdstan-2.16.0
     echo "Compiling CmdStan, this could take a while..."
     make build
     cd ..
-done
+fi
 # install python packages
 pip install -r requirements.txt
