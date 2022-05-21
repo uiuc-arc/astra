db1b726df2868023f4f02da8767120c150c78e98
yekerr <zixinh2@illinois.edu>
2022-05-21 15:56:08 -0500

    t

---
+++ build.sh
@@ -0,0 +1,6 @@
+#usr/bin/env bash
+
+mkdir ./cmdstan-2.16.0; tar -xf cmdstan-2.16.0.tar.gz -C ./cmdstan-2.16.0 --strip-components=1
+cd ./cmdstan-2.16.0
+echo "Compiling CmdStan, this could take a while..."
+make build
